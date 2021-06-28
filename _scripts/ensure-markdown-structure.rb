6f314c16035703255025be1f9d45dcf9562fd401
Eugene Obrezkov <ghaiklor@gmail.com>
2021-06-28 20:39:34 +0300

    [ci]: refactor ensure md structure script to be more customizable

--- _scripts/ensure-markdown-structure.rb
+++ _scripts/ensure-markdown-structure.rb
@@ -3,22 +3,19 @@ require 'front_matter_parser'
 languages = Dir.glob('??')
 sources = languages.map { |language| Dir.glob(language + '/' + '*.md') }.flatten
 errors = []
+fields = {
+  'index.md' => %w[title description keywords lang comments],
+  '*' => %w[id title lang level tags]
+}
 
 sources.each do |path|
   solution = FrontMatterParser::Parser.parse_file(path)
+  filename = Pathname.new(path).basename.to_s
+  fields_to_check = fields.key?(filename) ? fields.fetch(filename) : fields.fetch('*')
 
-  if path.end_with?('index.md')
-    errors.push("❌ [#{path}] has a missing title field") unless solution.front_matter.key?('title')
-    errors.push("❌ [#{path}] has a missing lang field") unless solution.front_matter.key?('lang')
-    errors.push("❌ [#{path}] has a missing comments field") unless solution.front_matter.key?('comments')
-    next
+  fields_to_check.each do |field|
+    errors.push("❌ [#{path}] has a missing field '#{field}'") unless solution.front_matter.key?(field)
   end
-
-  errors.push("❌ [#{path}] has a missing id field") unless solution.front_matter.key?('id')
-  errors.push("❌ [#{path}] has a missing title field") unless solution.front_matter.key?('title')
-  errors.push("❌ [#{path}] has a missing lang field") unless solution.front_matter.key?('lang')
-  errors.push("❌ [#{path}] has a missing level field") unless solution.front_matter.key?('level')
-  errors.push("❌ [#{path}] has a missing tags field") unless solution.front_matter.key?('tags')
 end
 
 print "\n--- Front Matter data in #{sources.size} files have been checked ---\n\n"
