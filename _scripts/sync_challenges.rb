9992538fa9f6b39d90f6f557feb5967e9736d772
Eugene Obrezkov <ghaiklor@gmail.com>
2022-06-19 14:04:41 +0300

    [linter]: apply rubocop to sync_challenges

--- _scripts/sync_challenges.rb
+++ _scripts/sync_challenges.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'pathname'
 require 'octokit'
 
-languages = Dir.glob('??').filter { |lang| ARGV[0].nil? ? true : lang === ARGV[0] }
+languages = Dir.glob('??').filter { |lang| ARGV[0].nil? ? true : lang == ARGV[0] }
 questions = Octokit::Client.new
                            .contents('type-challenges/type-challenges', path: 'questions')
-                           .map { |question| /\d+-(.+)/.match(question.name)[1] + '.md' }
+                           .map { |question| "#{/\d+-(.+)/.match(question.name)[1]}.md" }
                            .sort
 
 languages.each do |language|
   questions.each do |question|
     path = Pathname.new("#{language}/#{question}")
-    puts '❌ ' + path.to_s unless path.exist?
+    puts "❌ #{path}" unless path.exist?
   end
 end
